39db495b2797c53aeb92bbfbd53883cb6bc92b1f
arntmk <oleg.semchuk95@gmail.com>
2022-12-27 19:49:25 +0200

    add

--- src/js/libs/vendor.js
+++ src/js/libs/vendor.js
@@ -1,7 +1,1 @@
 // Custom Scripts imports
-
-// include('-1-title.js');
-// include('-2-title.js');
-// include('-3-title.js');
-// include('-4-title.js');
-// include('-5-title.js');
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 // Custom Scripts imports
 
 // include('./libs/vendor.js')
-// include('-1-index.js');
-// include('-2-meta-width-function.js');
+// include('index.js');
+// include('meta-width-function.js');
