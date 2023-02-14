a6e5391391d846a778ec648bf13bcb81f2c14e0f
arntmk <oleg.semchuk95@gmail.com>
2023-02-14 11:39:03 +0200

    add

--- src/js/_form.js
+++ src/js/_form.js
@@ -1,1 +1,17 @@
-// Custom Form Scripts imports
+// Custom Scripts imports
+
+// function NumberOnly(e) {
+// 	const keyCode = e.keyCode || e.which;
+// 	const NumberOnly = document.querySelectorAll('NumberOnly');
+// 	NumberOnly.innerHTML = '';
+
+// 	//Regex for Valid Characters i.e. Numbers.
+// 	const regex = /^[0-9.]+$/;
+
+// 	//Validate TextBox value against the Regex.
+// 	const isValid = regex.test(String.fromCharCode(keyCode));
+// 	// if (!isValid) {
+// 	// 		NumberOnly.innerHTML = "Only Numbers allowed.";
+// 	// }
+// 	return isValid;
+// }
