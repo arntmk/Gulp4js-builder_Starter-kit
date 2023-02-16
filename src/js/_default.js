7b79a76621c09b7d227b3e50a0e52e739bbb8b14
arntmk <oleg.semchuk95@gmail.com>
2023-02-16 14:16:26 +0200

    add

---
+++ src/js/_default.js
@@ -0,0 +1,27 @@
+// ===Toggle Password Visibility===
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+	const type =
+		password.getAttribute('type') === 'password' ? 'text' : 'password';
+	password.setAttribute('type', type);
+	this.classList.toggle('eye-close');
+});
+
+password.addEventListener('input', function () {
+	togglePassword.classList.toggle('hidden', !this.value);
+});
+
+// ===Toggle search cleaning===
+const searchInput = document.querySelector('#txtsearchinput');
+const clearButton = document.querySelector('#deltxtinput');
+
+clearButton.addEventListener('click', function () {
+	searchInput.value = '';
+	searchInput.dispatchEvent(new Event('input'));
+});
+
+searchInput.addEventListener('input', function () {
+	clearButton.classList.toggle('hidden', !this.value);
+});
