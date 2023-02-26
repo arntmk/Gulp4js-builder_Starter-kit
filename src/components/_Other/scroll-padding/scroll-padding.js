5fd915bd3ac1fd5fa5029400025c6d4beb186939
arntmk <oleg.semchuk95@gmail.com>
2023-02-26 13:07:54 +0200

    add

--- src/components/_Other/scroll-padding/scroll-padding.js
+++ src/components/_Other/scroll-padding/scroll-padding.js
@@ -4,3 +4,16 @@ document.documentElement.style.setProperty(
 	'--scroll-padding',
 	navigationHeight - 1 + 'px'
 );
+
+const minOffset = 50;
+window.onscroll = function () {
+	let has_class = document.body.classList.contains('is_scrolled');
+
+	if (minOffset < document.documentElement.scrollTop) {
+		if (!has_class) {
+			document.body.classList.add('is_scrolled');
+		}
+	} else if (has_class) {
+		document.body.classList.remove('is_scrolled');
+	}
+};
