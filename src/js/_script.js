c78032276e205fcbe6f3c7f3aa776c9ffb971b56
arntmk <oleg.semchuk95@gmail.com>
2023-02-26 23:13:15 +0200

    add

--- src/js/_script.js
+++ src/js/_script.js
@@ -1,1 +1,18 @@
 // Custom Scripts imports
+
+const accordions = document.querySelectorAll('details');
+
+accordions.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		const self = e.currentTarget;
+		const content = self.querySelector('.content');
+
+		self.classList.toggle('open');
+
+		if (self.classList.contains('open')) {
+			content.style.maxHeight = content.scrollHeight + 'px';
+		} else {
+			content.style.maxHeight = null;
+		}
+	});
+});
