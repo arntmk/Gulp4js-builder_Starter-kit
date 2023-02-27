123596c99f4410d2909630579b9f5a1977655c87
arntmk <oleg.semchuk95@gmail.com>
2023-02-27 20:55:31 +0200

    add

--- src/js/_default.js
+++ src/js/_default.js
@@ -1,98 +1,103 @@
 /* ____________________________________________ */
 // ===Toggle Password Visibility===
 
-const togglePassword = document?.querySelector('#togglePassword');
-const password = document?.querySelector('#password');
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
 
-togglePassword?.addEventListener('click', function (e) {
-	// toggle the type attribute
-	const type =
-		password.getAttribute('type') === 'password' ? 'text' : 'password';
-	password.setAttribute('type', type);
-	// toggle the eye / eye slash icon
-	this.classList.toggle('eye-close');
-});
+if (togglePassword && password) {
+	togglePassword.addEventListener('click', function (e) {
+		// toggle the type attribute
+		const type =
+			password.getAttribute('type') === 'password' ? 'text' : 'password';
+		password.setAttribute('type', type);
+		// toggle the eye / eye slash icon
+		this.classList.toggle('eye-close');
+	});
 
-password?.addEventListener('input', function () {
-	togglePassword.classList?.toggle('hidden', !this.value);
-});
-
+	password.addEventListener('input', function () {
+		togglePassword.classList.toggle('hidden', !this.value);
+	});
+}
 /* ____________________________________________ */
 // ===Toggle search cleaning===
 
-const searchInput = document?.querySelector('#txtsearchinput');
-const clearButton = document?.querySelector('#deltxtinput');
+const searchInput = document.querySelector('#txtsearchinput');
+const clearButton = document.querySelector('#deltxtinput');
 
-clearButton?.addEventListener('click', function () {
-	searchInput.value = '';
-	searchInput.dispatchEvent(new Event('input'));
-});
+if (searchInput && clearButton) {
+	clearButton.addEventListener('click', function () {
+		searchInput.value = '';
+		searchInput.dispatchEvent(new Event('input'));
+	});
 
-searchInput?.addEventListener('input', function () {
-	clearButton.classList?.toggle('hidden', !this.value);
-});
-
+	searchInput.addEventListener('input', function () {
+		clearButton.classList.toggle('hidden', !this.value);
+	});
+}
 /* ____________________________________________ */
 // ===Validate Numbers===
 
-const numericInputs = document?.querySelectorAll('[inputmode="numeric"]');
+const numericInputs = document.querySelectorAll('[inputmode="numeric"]');
 
-numericInputs.forEach((input) => {
-	validateInput(input);
-});
-
-function validateInput(el) {
-	el.addEventListener('beforeinput', function (e) {
-		let beforeValue = el.value;
-		e.target.addEventListener(
-			'input',
-			function () {
-				if (el.validity.patternMismatch) {
-					el.value = beforeValue;
-				}
-			},
-			{ once: true }
-		);
+if (numericInputs) {
+	numericInputs.forEach((input) => {
+		validateInput(input);
 	});
-}
 
-document.querySelector('[inputmode="numeric"]').onkeypress = function Validate(
-	e
-) {
-	const isNumber = /[0-9.]/.test(String.fromCharCode(e.keyCode || e.which));
-	return isNumber;
-};
+	function validateInput(el) {
+		el.addEventListener('beforeinput', function (e) {
+			let beforeValue = el.value;
+			e.target.addEventListener(
+				'input',
+				function () {
+					if (el.validity.patternMismatch) {
+						el.value = beforeValue;
+					}
+				},
+				{ once: true }
+			);
+		});
+	}
+
+	document.querySelector('[inputmode="numeric"]').onkeypress =
+		function Validate(e) {
+			const isNumber = /[0-9.]/.test(String.fromCharCode(e.keyCode || e.which));
+			return isNumber;
+		};
+}
 
 /* ____________________________________________ */
 // ==Accordion==
 
 const accordion = document.querySelectorAll('details');
 
-accordion.forEach((el) => {
-	el.addEventListener('click', (e) => {
-		const self = e.currentTarget;
-		const content = self.querySelector('.content');
+if (accordion) {
+	accordion.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			const self = e.currentTarget;
+			const content = self.querySelector('.content');
 
-		self.classList.toggle('open');
+			self.classList.toggle('open');
 
-		if (self.classList.contains('open')) {
-			content.style.maxHeight = content.scrollHeight + 'px';
-		} else {
-			content.style.maxHeight = null;
-		}
-	});
-});
-
-accordion.forEach((targetDetail) => {
-	targetDetail.addEventListener('click', () => {
-		accordion.forEach((detail) => {
-			if (detail !== targetDetail) {
-				detail.removeAttribute('open');
-				detail.classList.remove('open');
+			if (self.classList.contains('open')) {
+				content.style.maxHeight = content.scrollHeight + 'px';
+			} else {
+				content.style.maxHeight = null;
 			}
 		});
 	});
-});
+
+	accordion.forEach((targetDetail) => {
+		targetDetail.addEventListener('click', () => {
+			accordion.forEach((detail) => {
+				if (detail !== targetDetail) {
+					detail.removeAttribute('open');
+					detail.classList.remove('open');
+				}
+			});
+		});
+	});
+}
 
 /* ____________________________________________ */
 // ===Progress support JS / WebKit===
