02d15caf195e0712cba81beeab971764d3869568
arntmk <oleg.semchuk95@gmail.com>
2023-02-28 17:39:32 +0200

    add

---
+++ src/components/Modal/script.js
@@ -0,0 +1,27 @@
+/* ____________________________________________ */
+// ===Modal===
+
+const modal = document.querySelector('dialog');
+const openModal = document.querySelector('.open-btn');
+const closeModal = document.querySelector('.close-btn');
+
+const body = document.body;
+
+if (modal && openModal && closeModal) {
+	openModal.addEventListener('click', () => {
+		body.classList.toggle('lock');
+		modal.showModal();
+	});
+
+	closeModal.addEventListener('click', () => {
+		body.classList.remove('lock');
+		modal.close();
+	});
+
+	modal.addEventListener('click', (event) => {
+		if (event.target === modal) {
+			body.classList.remove('lock');
+			modal.close();
+		}
+	});
+}
