00ac5eb32bbdf223f20f9461900d67b0a8c65be8
arntmk <oleg.semchuk95@gmail.com>
2023-03-03 13:42:55 +0200

    add

--- src/components/Select/srcipt.js
+++ src/components/Select/srcipt.js
@@ -9,7 +9,8 @@ const options = document.querySelectorAll('.option');
 select.addEventListener('click', () => {
 	select.classList.toggle('active');
 	options_list.classList.toggle('active');
-	// select.querySelector('.fa-angle-down').classList.toggle('fa-angle-up');
+	select.setAttribute('aria-expanded', true);
+	options_list.setAttribute('aria-hidden', false);
 });
 
 //select option
@@ -17,10 +18,12 @@ options.forEach((option) => {
 	option.addEventListener('click', () => {
 		options.forEach((option) => {
 			option.classList.remove('selected');
+			select.setAttribute('aria-expanded', false);
+			options_list.setAttribute('aria-hidden', true);
 		});
+
 		select.querySelector('span').innerHTML = option.innerHTML;
 		option.classList.add('selected');
 		options_list.classList.toggle('active');
-		// select.querySelector('.fa-angle-down').classList.toggle('fa-angle-up');
 	});
 });
