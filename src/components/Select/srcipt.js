22e8c43e7ee7ca2730f52f21014d1a9f4ce27ae6
arntmk <oleg.semchuk95@gmail.com>
2023-03-03 15:56:33 +0200

    add

--- src/components/Select/srcipt.js
+++ src/components/Select/srcipt.js
@@ -5,32 +5,34 @@ const select = document.querySelector('.select');
 const options_list = document.querySelector('.options-list');
 const options = document.querySelectorAll('.option');
 
-//show & hide options list
-select.addEventListener('click', () => {
-	select.classList.toggle('active');
-	options_list.classList.toggle('active');
+if (select && options_list && options) {
+	//show & hide options list
+	select.addEventListener('click', () => {
+		select.classList.toggle('active');
+		options_list.classList.toggle('active');
 
-	if (select.classList.contains('active')) {
-		select.setAttribute('aria-expanded', true);
-		options_list.setAttribute('aria-hidden', false);
-	} else {
-		select.setAttribute('aria-expanded', false);
-		options_list.setAttribute('aria-hidden', true);
-	}
-});
-
-//select option
-options.forEach((option) => {
-	option.addEventListener('click', () => {
-		options.forEach((option) => {
-			option.classList.remove('selected');
-			select.classList.remove('active');
+		if (select.classList.contains('active')) {
+			select.setAttribute('aria-expanded', true);
+			options_list.setAttribute('aria-hidden', false);
+		} else {
 			select.setAttribute('aria-expanded', false);
 			options_list.setAttribute('aria-hidden', true);
-		});
-
-		select.querySelector('span').innerHTML = option.innerHTML;
-		option.classList.add('selected');
-		options_list.classList.toggle('active');
+		}
 	});
-});
+
+	//select option
+	options.forEach((option) => {
+		option.addEventListener('click', () => {
+			options.forEach((option) => {
+				option.classList.remove('selected');
+				select.classList.remove('active');
+				select.setAttribute('aria-expanded', false);
+				options_list.setAttribute('aria-hidden', true);
+			});
+
+			select.querySelector('span').innerHTML = option.innerHTML;
+			option.classList.add('selected');
+			options_list.classList.toggle('active');
+		});
+	});
+}
