ea26f322ca1e26cbf2e4fdc05a8ba4324eccade2
arntmk <oleg.semchuk95@gmail.com>
2023-03-03 12:05:56 +0200

    add

---
+++ src/components/_Other_/PhoneMask/script.js
@@ -0,0 +1,12 @@
+// https://www.youtube.com/watch?v=3bQO_sVFXvM&t=287s
+// Сылька на плагин https://imask.js.org/
+// Подключаем через npm: https://www.npmjs.com/package/imask
+// Подключаем через CDN: <script src="https://unpkg.com/imask"></script>
+
+/* ____________________________________________ */
+// Init phonemask
+const maskElement = document.querySelector('#phone__input');
+const maskOption = {
+	mask: '+{38}(000)000-00-00',
+};
+const mask = IMask(maskElement, maskOption);
