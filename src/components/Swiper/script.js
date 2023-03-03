076f47b24d5b83ff13e8a91042b980e2b2858056
arntmk <oleg.semchuk95@gmail.com>
2023-03-03 08:37:13 +0200

    add

--- src/components/Swiper/script.js
+++ src/components/Swiper/script.js
@@ -1,10 +1,34 @@
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
-	// direction: 'vertical',
+	direction: 'horizontal', // vertical
 	loop: true,
 	spaceBetween: 20,
-	slidesPerView: 2,
+	slidesPerView: 3,
+	slidesPerGroup: 1,
 	autoHeight: true,
+	speed: 300,
+
+	autoplay: {
+		delay: 5000,
+	},
+
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		// when window width is >= 480px
+		480: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		// when window width is >= 640px
+		640: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+	},
 
 	// Navigation arrows
 	navigation: {
@@ -15,6 +39,7 @@ const swiper = new Swiper('.swiper', {
 	// If we need pagination
 	// pagination: {
 	// 	el: '.swiper-pagination',
+	// 	type: 'bullets',
 	// 	clickable: true,
 	// },
 
