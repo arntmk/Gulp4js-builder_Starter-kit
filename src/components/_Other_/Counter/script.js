7f7b36b239c0578ee5a9897f6119c0a236b7e41a
arntmk <oleg.semchuk95@gmail.com>
2023-03-03 11:34:34 +0200

    add

---
+++ src/components/_Other_/Counter/script.js
@@ -0,0 +1,27 @@
+/* ____________________________________________ */
+// ===Number counter===
+function numCounter(selector, number, time, step) {
+	const counter = document.querySelector(selector);
+
+	let res = 0;
+
+	const allTime = Math.round(time / (number / step));
+
+	let interval = setInterval(() => {
+		res = res + step;
+
+		if (res === number) {
+			clearInterval(interval);
+		}
+		counter.innerHTML = res;
+	}, allTime);
+}
+
+// ДАННАЯ ФУНКЦИЯ МОЖЕТ БЫТЬ ВЫЗВАННА НЕОГРАНИЧЕНОЕ КОЛИЧЕСТВО РАЗ
+
+// Первий аргумент - селектор, куда будем выводить результат #num or .num
+// Втарой аргумент - конечное значение которое будет показано на странице.
+// Третий аргумент - время анимации (милисекунды)
+// Четвертый аргумент - шаг анимации (например добавляем по 1 или по 18 или по 180)
+
+numCounter('#num1', 600, 2000, 10);
