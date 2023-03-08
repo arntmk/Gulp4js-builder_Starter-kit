ba29f214e26144a2bf45c229ec2249b5a0413b5e
arntmk <oleg.semchuk95@gmail.com>
2023-03-08 13:09:38 +0200

    add

--- src/components/Swiper/script.js
+++ src/components/Swiper/script.js
@@ -3,30 +3,38 @@
 /* ____________________________________________ */
 // ===Swiper===
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-container', {
 	// Optional parameters
 	direction: 'horizontal', // vertical
 	loop: true,
 	spaceBetween: 20,
 	slidesPerView: 3,
 	slidesPerGroup: 1,
+	//slideToClickedSlide: true,
+	//centeredSlides: true,
 	autoHeight: true,
 	speed: 300,
+	freeMode: true,
 
 	autoplay: {
 		delay: 5000,
 	},
 
 	breakpoints: {
+		// when window width is >= 50px
+		50: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+		},
 		// when window width is >= 320px
 		320: {
 			slidesPerView: 1,
-			spaceBetween: 10,
+			spaceBetween: 0,
 		},
 		// when window width is >= 480px
 		480: {
 			slidesPerView: 2,
-			spaceBetween: 20,
+			spaceBetween: 10,
 		},
 		// when window width is >= 640px
 		640: {
@@ -42,11 +50,12 @@ const swiper = new Swiper('.swiper', {
 	},
 
 	// If we need pagination
-	// pagination: {
-	// 	el: '.swiper-pagination',
-	// 	type: 'bullets',
-	// 	clickable: true,
-	// },
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		paginationClickable: true,
+		clickable: true,
+	},
 
 	// And if we need scrollbar
 	// scrollbar: {
