e6648d89478891d154ca050cf418db8c5192a360
arntmk <oleg.semchuk95@gmail.com>
2023-03-08 15:41:32 +0200

    add

--- src/components/Swiper/script.js
+++ src/components/Swiper/script.js
@@ -3,57 +3,62 @@
 /* ____________________________________________ */
 // ===Swiper===
 
-const swiper = new Swiper('.swiper-container', {
-	// Optional parameters
-	direction: 'horizontal', // vertical
-	loop: true,
-	spaceBetween: 20,
-	slidesPerView: 1,
-	slidesPerGroup: 1,
-	//slideToClickedSlide: true,
-	//centeredSlides: true,
-	autoHeight: true,
-	speed: 300,
-	freeMode: true,
+const sliders = document.querySelectorAll('.swiper-container');
 
-	autoplay: {
-		delay: 4000,
-	},
+sliders.forEach((el) => {
+	let swiper = new Swiper(el, {
+		// Optional parameters
+		direction: 'horizontal', // vertical
+		loop: true,
+		spaceBetween: 20,
+		slidesPerView: 1,
+		slidesPerGroup: 1,
+		//slideToClickedSlide: true,
+		//centeredSlides: true,
+		autoHeight: true,
+		speed: 300,
+		freeMode: true,
+		//effect: fade,
 
-	breakpoints: {
-		// when window width is >= 320px
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 0,
+		autoplay: {
+			delay: 4000,
 		},
-		// when window width is >= 480px
-		480: {
-			slidesPerView: 2,
-			spaceBetween: 10,
+
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+				slidesPerView: 1,
+				spaceBetween: 0,
+			},
+			// when window width is >= 480px
+			480: {
+				slidesPerView: 2,
+				spaceBetween: 10,
+			},
+			// when window width is >= 640px
+			640: {
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
 		},
-		// when window width is >= 640px
-		640: {
-			slidesPerView: 3,
-			spaceBetween: 20,
+
+		// Navigation arrows
+		navigation: {
+			nextEl: el.querySelector('.swiper-button-next'),
+			prevEl: el.querySelector('.swiper-button-prev'),
 		},
-	},
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+		// If we need pagination
+		pagination: {
+			el: el.querySelector('.swiper-pagination'),
+			type: 'bullets', // fraction
+			paginationClickable: true,
+			clickable: true,
+		},
 
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-		type: 'bullets', // fraction
-		paginationClickable: true,
-		clickable: true,
-	},
-
-	// And if we need scrollbar
-	// scrollbar: {
-	//   el: '.swiper-scrollbar',
-	// },
+		// And if we need scrollbar
+		// scrollbar: {
+		// 	el: el.querySelector('.swiper-scrollbar'),
+		// },
+	});
 });
