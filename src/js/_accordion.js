1366e40db1eac58683bff6c9ef2db86bf5e22c0e
arntmk <oleg.semchuk95@gmail.com>
2023-05-02 12:45:07 +0300

    'commite.'

---
+++ src/js/_accordion.js
@@ -0,0 +1,32 @@
+/* ____________________________________________ */
+// ===Accordion===
+
+const accordion = document.querySelectorAll('details');
+
+if (accordion) {
+	accordion.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			const self = e.currentTarget;
+			const content = self.querySelector('.content');
+
+			self.classList.toggle('open');
+
+			if (self.classList.contains('open')) {
+				content.style.maxHeight = `${content.scrollHeight}px`;
+			} else {
+				content.style.maxHeight = null;
+			}
+		});
+	});
+
+	accordion.forEach((targetDetail) => {
+		targetDetail.addEventListener('click', () => {
+			accordion.forEach((detail) => {
+				if (detail !== targetDetail) {
+					detail.removeAttribute('open');
+					detail.classList.remove('open');
+				}
+			});
+		});
+	});
+}
--- src/js/default/_default.js
+++ src/js/default/_default.js
@@ -43,38 +43,6 @@ document.querySelector('[inputmode="numeric"]').onkeypress = function Validate(e
 	return isNumber;
 };
 
-/* ____________________________________________ */
-// ===Accordion===
-
-const accordion = document.querySelectorAll('details');
-
-if (accordion) {
-	accordion.forEach((el) => {
-		el.addEventListener('click', (e) => {
-			const self = e.currentTarget;
-			const content = self.querySelector('.content');
-
-			self.classList.toggle('open');
-
-			if (self.classList.contains('open')) {
-				content.style.maxHeight = `${content.scrollHeight}px`;
-			} else {
-				content.style.maxHeight = null;
-			}
-		});
-	});
-
-	accordion.forEach((targetDetail) => {
-		targetDetail.addEventListener('click', () => {
-			accordion.forEach((detail) => {
-				if (detail !== targetDetail) {
-					detail.removeAttribute('open');
-					detail.classList.remove('open');
-				}
-			});
-		});
-	});
-}
 
 /* ____________________________________________ */
 // ===Progress support JS / WebKit===
