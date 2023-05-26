f02e9cc507f5acfcfef82997631ec724a7f2fcc6
arntmk <oleg.semchuk95@gmail.com>
2023-05-26 17:50:02 +0300

    'commite.'

---
+++ bower_components/Modal/native/script.js
@@ -0,0 +1,27 @@
+/* ____________________________________________ */
+// ===Modal===
+
+const modal = document.querySelector('dialog');
+const openModal = document.querySelector('[data-open-modal]');
+const closeModal = document.querySelectorAll('[data-close-modal]');
+
+const { body } = document;
+
+if (modal && openModal && closeModal) {
+	openModal.addEventListener('click', () => {
+		body.classList.toggle('lock');
+		modal.showModal();
+	});
+
+	closeModal.forEach((item) => item.addEventListener('click', () => {
+		body.classList.remove('lock');
+		modal.close();
+	}));
+
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			body.classList.remove('lock');
+			modal.close();
+		}
+	});
+}
