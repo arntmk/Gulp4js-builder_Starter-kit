07d9680fcf0b1de5321b70b0ce056a5de5b49683
arntmk <oleg.semchuk95@gmail.com>
2023-05-30 13:28:47 +0300

    'commite.'

--- bower_components/Tabs/button/script.js
+++ bower_components/Tabs/button/script.js
@@ -8,12 +8,14 @@ if (tabsNav && tabsContent) {
 		const currentTab = document.querySelector(tabId);
 
 		if (!currentBtn.classList.contains('active')) {
-			tabsNav.forEach((child) => { child.classList.remove('active'); });
+			tabsNav.forEach((child) => { child.classList.remove('active'); child.setAttribute('aria-expanded', false); });
 
-			tabsContent.forEach((child) => { child.classList.remove('active'); });
+			tabsContent.forEach((child) => { child.classList.remove('active'); child.setAttribute('aria-hidden', true); });
 
 			currentBtn.classList.add('active');
 			currentTab.classList.add('active');
+			currentBtn.setAttribute('aria-expanded', true);
+			currentTab.setAttribute('aria-hidden', false);
 		}
 	}));
 	document.querySelector('.tabs__nav-btn').click();
--- bower_components/Tabs/link/script.js
+++ bower_components/Tabs/link/script.js
@@ -9,11 +9,13 @@ if (tabsNav && tabsContent) {
 		e.preventDefault();
 		const id = e.target.getAttribute('href').replace('#', '');
 
-		tabsNav.forEach((child) => child.classList.remove('active'));
-		tabsContent.forEach((child) => child.classList.remove('active'));
+		tabsNav.forEach((child) => { child.classList.remove('active'); child.setAttribute('aria-expanded', false); });
+		tabsContent.forEach((child) => { child.classList.remove('active'); child.setAttribute('aria-hidden', true); });
 
 		item.classList.add('active');
 		document.getElementById(id).classList.add('active');
+		item.setAttribute('aria-expanded', true);
+		document.getElementById(id).setAttribute('aria-hidden', false);
 	}));
 	document.querySelector('.tabs__nav-btn').click();
 }
