1b857e456b7711fd7455074e2cd04556d44c8d34
arntmk <oleg.semchuk95@gmail.com>
2023-06-01 16:57:05 +0300

    'commite.'

--- src/js/modules/_body-min-width.js
+++ src/js/modules/_body-min-width.js
@@ -1,9 +1,12 @@
 // ===Adaptive-body-min-width===
-(function () {
-	const { width } = window.screen;
-	const oldViewport = document.querySelector('meta[name="viewport"]');
-	const viewport = document.createElement('meta');
-	viewport.setAttribute('name', 'viewport');
-	viewport.setAttribute('content', `width=${width <= 320 ? '320' : 'device-width'}`);
-	document.head.replaceChild(viewport, oldViewport);
-}());
+const screenViewport = function () {
+	(function () {
+		const { width } = window.screen;
+		const oldViewport = document.querySelector('meta[name="viewport"]');
+		const viewport = document.createElement('meta');
+		viewport.setAttribute('name', 'viewport');
+		viewport.setAttribute('content', `width=${width <= 320 ? '320' : 'device-width'}`);
+		document.head.replaceChild(viewport, oldViewport);
+	}());
+};
+screenViewport();
