88d4a850e48ce6c7fb640d518d75b611f84d7cf6
arntmk <oleg.semchuk95@gmail.com>
2023-06-01 16:58:23 +0300

    'commite.'

--- src/assets/default/_accordion.js
+++ src/assets/default/_accordion.js
@@ -1,38 +1,41 @@
 /* ____________________________________________ */
 // ===Accordion===
 
-const accordion = document.querySelectorAll('details');
+const accordionsController = function () {
+	const accordion = document.querySelectorAll('details');
 
-if (accordion) {
-	accordion.forEach((el) => {
-		el.addEventListener('click', (e) => {
-			const self = e.currentTarget;
-			const content = self.querySelector('.content');
+	if (accordion) {
+		accordion.forEach((el) => {
+			el.addEventListener('click', (e) => {
+				const self = e.currentTarget;
+				const content = self.querySelector('.content');
 
-			if (self.hasAttribute('open')) {
-				content.style.maxHeight = null;
-			} else {
-				content.style.maxHeight = `${content.scrollHeight}`;
-			}
-		});
-	});
-
-	// Клик снаружи аркадиона. Закрыть аркадион.
-	accordion.forEach((closeDetails) => {
-		document.addEventListener('click', (e) => {
-			if (!closeDetails.contains(e.target)) {
-				closeDetails.removeAttribute('open');
-			}
-		});
-	});
-
-	accordion.forEach((targetDetails) => {
-		targetDetails.addEventListener('click', () => {
-			accordion.forEach((detail) => {
-				if (detail !== targetDetails) {
-					detail.removeAttribute('open');
+				if (self.hasAttribute('open')) {
+					content.style.maxHeight = null;
+				} else {
+					content.style.maxHeight = `${content.scrollHeight}`;
 				}
 			});
 		});
-	});
-}
+
+		// Клик снаружи аркадиона. Закрыть аркадион.
+		accordion.forEach((closeDetails) => {
+			document.addEventListener('click', (e) => {
+				if (!closeDetails.contains(e.target)) {
+					closeDetails.removeAttribute('open');
+				}
+			});
+		});
+
+		accordion.forEach((targetDetails) => {
+			targetDetails.addEventListener('click', () => {
+				accordion.forEach((detail) => {
+					if (detail !== targetDetails) {
+						detail.removeAttribute('open');
+					}
+				});
+			});
+		});
+	}
+};
+accordionsController();
