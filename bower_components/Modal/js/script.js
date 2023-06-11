85d7f028a8870ee0b62fd35e4a61994d44064f11
arntmk <oleg.semchuk95@gmail.com>
2023-06-11 19:33:05 +0300

    'commite.'

--- bower_components/Modal/js/script.js
+++ bower_components/Modal/js/script.js
@@ -2,24 +2,26 @@
 
 /* ____________________________________________ */
 // ===Disable Scroll===
+const headerFixed = document.querySelector('.header');
 const { body } = document;
+const { documentElement } = document;
 
 const scrollController = {
 	scrollPosition: 0,
 	disableScroll() {
 		scrollController.scrollPosition = window.scrollY;
-		document.body.style.cssText = `
+		body.style.cssText = `
 		top: -${scrollController.scrollPosition}px;
 		padding-right: ${window.innerWidth - document.body.offsetWidth}px
 		`;
-		document.documentElement.style.cssText = 'scroll-behavior: unset; scrollbar-gutter: stable;';
+		documentElement.style.cssText = 'scroll-behavior: unset; scrollbar-gutter: stable;';
 		body.classList.toggle('lock');
 	},
 	enableScroll() {
 		body.classList.remove('lock');
-		document.body.style.cssText = '';
+		body.style.cssText = '';
 		window.scroll({ top: scrollController.scrollPosition });
-		document.documentElement.style.cssText = '';
+		documentElement.style.cssText = '';
 	},
 };
 
