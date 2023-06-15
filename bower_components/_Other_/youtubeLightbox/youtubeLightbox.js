9e216784afc86974d9a0633b0ff69be391424cbd
arntmk <oleg.semchuk95@gmail.com>
2023-06-15 11:41:08 +0300

    'commite.'

--- bower_components/_Other_/youtubeLightbox/youtubeLightbox.js
+++ bower_components/_Other_/youtubeLightbox/youtubeLightbox.js
@@ -3,16 +3,16 @@ const linksBtnsSelector = 'a[data-youtubeLightbox]';
 // const linksBtnsSelector = '.lightbox';
 
 // load Youtube API code asynchronously
-var tag = document.createElement('script');
+const tag = document.createElement('script');
 
 tag.src = 'https://www.youtube.com/iframe_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
+const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var isiOS = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)/i) != null; //boolean check for iOS devices
+const isiOS = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)/i) != null; // boolean check for iOS devices
 
-var youtubelightbox = document.getElementById('youtubelightbox');
-var player; // variable to hold new YT.Player() instance
+const youtubelightbox = document.getElementById('youtubelightbox');
+let player; // variable to hold new YT.Player() instance
 
 // Hide lightbox when clicked on
 youtubelightbox.addEventListener(
@@ -21,19 +21,17 @@ youtubelightbox.addEventListener(
 		this.style.display = 'none';
 		player.stopVideo();
 	},
-	false
+	false,
 );
 
 // Exclude youtube iframe from above action
-youtubelightbox
-	.querySelector('.youtubelightbox__centeredchild')
-	.addEventListener(
-		'click',
-		function (e) {
-			e.stopPropagation();
-		},
-		false
-	);
+youtubelightbox.querySelector('.youtubelightbox__centeredchild').addEventListener(
+	'click',
+	(e) => {
+		e.stopPropagation();
+	},
+	false,
+);
 
 // define onYouTubeIframeAPIReady() function and initialize lightbox when API is ready
 function onYouTubeIframeAPIReady() {
@@ -48,8 +46,8 @@ function getyoutubeid(link) {
 	// https://www.youtube.com/v/Pe0jFDPHkzo
 	// and more
 
-	//See http://stackoverflow.com/a/6904504/4360074
-	var youtubeidreg =
+	// See http://stackoverflow.com/a/6904504/4360074
+	const youtubeidreg =
 		/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
 	return youtubeidreg.exec(link)[1]; // return Youtube video ID portion of link
 }
@@ -64,29 +62,27 @@ function createyoutubeplayer(videourl) {
 
 // Main Youtube lightbox function
 function createlightbox() {
-	var targetlinks = document.querySelectorAll(linksBtnsSelector);
+	const targetlinks = document.querySelectorAll(linksBtnsSelector);
 
-	for (var i = 0; i < targetlinks.length; i++) {
-		var link = targetlinks[i];
+	for (let i = 0; i < targetlinks.length; i++) {
+		const link = targetlinks[i];
 		link._videoid = getyoutubeid(link); // store youtube video ID portion of link inside _videoid property
 		targetlinks[i].addEventListener(
 			'click',
 			function (e) {
 				youtubelightbox.style.display = 'block';
-				if (typeof player == 'undefined') {
+				if (typeof player === 'undefined') {
 					// if video player hasn't been created yet
 					createyoutubeplayer(this._videoid);
+				} else if (isiOS) {
+					// iOS devices can only use the "cue" related methods
+					player.cueVideoById(this._videoid);
 				} else {
-					if (isiOS) {
-						// iOS devices can only use the "cue" related methods
-						player.cueVideoById(this._videoid);
-					} else {
-						player.loadVideoById(this._videoid);
-					}
+					player.loadVideoById(this._videoid);
 				}
 				e.preventDefault();
 			},
-			false
+			false,
 		);
 	}
 }
