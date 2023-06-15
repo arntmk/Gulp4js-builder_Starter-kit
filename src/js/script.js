df891db967082541dbe2c77bffb81bd6c53c46ba
arntmk <oleg.semchuk95@gmail.com>
2023-06-15 07:49:05 +0300

    'commite.'

--- src/js/script.js
+++ src/js/script.js
@@ -18,21 +18,32 @@ const scrollController = {
 	scrollPosition: 0,
 	disableScroll() {
 		scrollController.scrollPosition = window.scrollY;
-		body.style.cssText = `
+		if (navigator.userAgent.indexOf('Firefox') === -1) {
+			body.style.cssText = `
 		top: -${scrollController.scrollPosition}px;
 		padding-right: ${window.innerWidth - document.body.offsetWidth}px
+		`;
+		}
+		body.style.paddingRight = `${window.innerWidth - document.body.offsetWidth}px
 		`;
 		if (style.position === 'fixed' || style.getPropertyValue('position') === 'absolute') {
 			headerFixed.style.paddingRight = `${window.innerWidth - document.body.offsetWidth}px
 		`;
 		}
-		documentElement.style.cssText = 'scroll-behavior: unset; scrollbar-gutter: unset;';
-		body.classList.toggle('lock');
+		documentElement.style.cssText = 'scroll-behavior: unset;';
+		if (navigator.userAgent.indexOf('Firefox') === -1) {
+			body.classList.toggle('lock-fixed');
+		}
+		if (navigator.userAgent.indexOf('Firefox') !== -1) {
+			body.classList.toggle('lock');
+		}
 	},
 	enableScroll() {
 		setTimeout(() => {
+			body.classList.remove('lock-fixed');
 			body.classList.remove('lock');
 			body.style.cssText = '';
+			body.style.paddingRight = '';
 			headerFixed.style.paddingRight = '';
 			window.scroll({ top: scrollController.scrollPosition });
 			documentElement.style.cssText = '';
