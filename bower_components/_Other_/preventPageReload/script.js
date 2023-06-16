384a134eb54b426036e306ee75ee6f2043fae4ef
arntmk <oleg.semchuk95@gmail.com>
2023-06-16 13:12:42 +0300

    'commite.'

--- bower_components/_Other_/preventPageReload/script.js
+++ bower_components/_Other_/preventPageReload/script.js
@@ -1,7 +1,19 @@
 /* https://www.tutorialspoint.com/how-to-stop-refreshing-the-page-on-submit-in-javascript */
+
 /* ____________________________________________ */
 // ===prevent page reload on submit===
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 });
+
+/* ____________________________________________ */
+// ===prevent page reload on submit===
+/* const form = document.querySelector('form');
+const submit = document.querySelector('.submit');
+function submitForm(e) {
+	e.preventDefault();
+	form.style.display = 'none';
+	submit.innerHTML = '<b>Form submit successful</b>';
+}
+form.addEventListener('submit', submitForm); */
