9935d33200c3dd0732969e7f93a3489df9e6cf28
arntmk <oleg.semchuk95@gmail.com>
2023-06-21 11:02:44 +0300

    'commite.'

--- bower_components/_Other_/Different/script.js
+++ bower_components/_Other_/Different/script.js
@@ -4,6 +4,30 @@
 } */
 
 /* ____________________________________________ */
+/* https://www.tutorialspoint.com/how-to-stop-refreshing-the-page-on-submit-in-javascript */
+/* https://timetoprogram.com/prevent-page-reload-form-submit-react-js/ */
+/* https://stackoverflow.com/questions/14589193/clearing-my-form-inputs-after-submission */
+/* ____________________________________________ */
+// ===prevent page reload on submit===
+const form = document.querySelectorAll('form');
+if (form) {
+	form.forEach((forms) => {
+		forms.addEventListener('submit', (e) => {
+			e.preventDefault();
+		});
+	});
+}
+
+/* ____________________________________________ */
+// ===prevent page to scroll up===
+const link = document.querySelectorAll('a[href="#"]');
+link.forEach((links) => {
+	links.addEventListener('click', (e) => {
+		e.preventDefault();
+	});
+});
+
+/* ____________________________________________ */
 // ===disable scroll===
 const headerFixed = document.querySelector('.header');
 const headerStyle = window.getComputedStyle(headerFixed);
@@ -47,30 +71,6 @@ const scrollController = {
 		}, 400);
 	},
 };
-
-/* ____________________________________________ */
-/* https://www.tutorialspoint.com/how-to-stop-refreshing-the-page-on-submit-in-javascript */
-/* https://timetoprogram.com/prevent-page-reload-form-submit-react-js/ */
-/* https://stackoverflow.com/questions/14589193/clearing-my-form-inputs-after-submission */
-/* ____________________________________________ */
-// ===prevent page reload on submit===
-const form = document.querySelectorAll('form');
-if (form) {
-	form.forEach((forms) => {
-		forms.addEventListener('submit', (e) => {
-			e.preventDefault();
-		});
-	});
-}
-
-/* ____________________________________________ */
-// ===prevent page to scroll up===
-const link = document.querySelectorAll('a[href="#"]');
-link.forEach((links) => {
-	links.addEventListener('click', (e) => {
-		e.preventDefault();
-	});
-});
 
 /* ____________________________________________ */
 // https://codepen.io/vaskort/pen/LYpwjoj
