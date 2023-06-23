6f6e60777d2705dc2579a3b1f300f16191a3a379
arntmk <oleg.semchuk95@gmail.com>
2023-06-23 11:10:20 +0300

    'commite.'

--- src/script.js
+++ src/script.js
@@ -6,11 +6,11 @@
 const JsFiles = [
 	'./src/js/script.js',
 
-	'./src/assets/default/_forms.js',
-	'./src/assets/default/_accordion.js',
-
 	'./src/js/modules/_body-min-width.js',
 	'./src/js/modules/_hamburger.js',
+
+	'./src/assets/default/_forms.js',
+	'./src/assets/default/_accordion.js',
 
 	'./src/js/modules/**/_*.{js,ts}',
 	'./src/components/**/_*.{js,ts}',
