88a66eb82864ca8eee79cedd7fc22d0d2bd16c0e
arntmk <oleg.semchuk95@gmail.com>
2023-06-26 16:37:39 +0300

    'commite.'

--- src/script.js
+++ src/script.js
@@ -2,13 +2,13 @@
 
 /* Base JS imports */
 import './js/main';
-import './js/modules/_body-min-width';
+import './js/_adap-body';
 
 /* Default */
 import './assets/default/_forms';
 import './assets/default/_accordion';
 
 /* Components JS files imports */
-import './js/modules/_hamburger';
+import './js/components/_hamburger';
 
 /* Libs */
