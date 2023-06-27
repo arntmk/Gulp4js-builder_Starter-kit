6a612c47619cbcdcb4557dcda73eb1cdd33724b2
arntmk <oleg.semchuk95@gmail.com>
2023-06-27 13:17:11 +0300

    'commite.'

--- gulpfile.js
+++ gulpfile.js
@@ -239,8 +239,9 @@ function webp() {
 
 function html() {
 	const copyFavicon = `${srcFolder}/assets/*.{png,ico,txt}`;
-	return src(`${srcFolder}/*.html`)
+	return src([`${srcFolder}/*.html`, `${srcFolder}/**/_*.html`])
 		.pipe(plumber())
+		.pipe(gulpif(isDev, changed(`${buildFolder}/`, { extension: '.html' })))
 		.pipe(fileinclude({ prefix: '@' }))
 		.pipe(
 			typograf({
