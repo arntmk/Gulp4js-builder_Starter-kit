77c1e72cbe1ba1ac4bdac9f4904665437dcbdc08
arntmk <oleg.semchuk95@gmail.com>
2023-07-14 14:06:31 +0300

    'commite.'

--- src/scripts/export/_trap-focus.js
+++ src/scripts/export/_trap-focus.js
@@ -6,7 +6,7 @@
 const trapFocus = (element) => {
 	const focusableEls = Array.from(
 		element.querySelectorAll(
-			'a[href]:not([disabled]), button:not([disabled]), input:not([disabled]), textarea:not([disabled]), select:not([disabled]), details:not([disabled]), [tabindex]:not([tabindex="-1"]), audio[controls], video[controls], [contenteditable]:not([contenteditable="false"])',
+			'a[href]:not([disabled]), button:not([disabled]), input:not([disabled]), textarea:not([disabled]), select:not([disabled]), details:not([disabled]), [tabindex]:not([tabindex="0"]), audio[controls], video[controls], [contenteditable]:not([contenteditable="false"])',
 		),
 	);
 	const firstFocusableEl = focusableEls[0];
