567b204109eb242238e9552839b2fb1632e82fab
arntmk <oleg.semchuk95@gmail.com>
2023-07-18 16:34:47 +0300

    'commite.'

--- src/script.js
+++ src/script.js
@@ -4,12 +4,12 @@
 import './scripts/_adap-body';
 import './scripts/main';
 
+/* Components JS imports */
+import './components/block-js/_hamburger';
+
 /* Tests */
 import './assets/tests/js/_forms';
 import './assets/tests/js/_accordion';
-
-/* Components JS imports */
-import './components/block-js/_hamburger';
 
 /* Libs */
 // import './scripts/libs/swiper-bundle.min';
