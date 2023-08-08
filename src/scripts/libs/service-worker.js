cf2e856e389060f54bc6a797d7d4c7a9f4f55926
arntmk <oleg.semchuk95@gmail.com>
2023-08-08 15:57:07 +0300

    update _head.twig and service-worker.js

--- src/scripts/libs/service-worker.js
+++ src/scripts/libs/service-worker.js
@@ -1,2 +1,19 @@
+/* eslint-disable no-console */
+/* eslint-disable no-restricted-globals */
+// https://web.dev/codelab-make-installable/
+// https://developer.mozilla.org/en-US/docs/Web/API/BeforeInstallPromptEvent
 /* ____________________________________________ */
 // ===Service Worker API===
+navigator.serviceWorker.register('/service-worker.js');
+
+window.addEventListener('load', () => {
+	self.addEventListener('fetch', (e) => {
+		console.log(e.request.url);
+	});
+});
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+	e.preventDefault();
+	deferredPrompt = e;
+});
