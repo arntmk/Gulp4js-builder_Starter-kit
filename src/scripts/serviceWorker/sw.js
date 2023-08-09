6b54ba26bde98d5f88234cb4fbfc978d8c85e4c3
arntmk <oleg.semchuk95@gmail.com>
2023-08-09 15:39:43 +0300

    update sw.js

--- src/scripts/serviceWorker/sw.js
+++ src/scripts/serviceWorker/sw.js
@@ -3,29 +3,48 @@
 /* ____________________________________________ */
 // https://web.dev/codelab-make-installable/
 // https://developer.mozilla.org/en-US/docs/Web/API/BeforeInstallPromptEvent
+// https://www.youtube.com/watch?v=ifroMW_F4Sc
+/* ____________________________________________ */
+// console.log('[SW]: install');
+// console.log('[SW]: activate');
+// console.log('fetch', e.request.url);
 /* ____________________________________________ */
 // ===Service Worker=== //
 
 // Cache
 const staticCacheName = 's-app-v1';
-const assetUrls = ['/index.html'];
+const dynamicCacheName = 'd-app-v1';
+const assetUrls = ['/index.html', '/offline.html'];
 
 // Files Cache
 self.addEventListener('install', (e) => {
 	e.waitUntil(caches.open(staticCacheName).then((cache) => cache.addAll(assetUrls)));
+
+	// async
+	// const cache = await caches.open(staticCacheName);
+	// await cache.addAll(assetUrls);
 });
 
 // Clear Cache
-self.addEventListener('activate', async (e) => {
+self.addEventListener('activate', async () => {
 	const cachedNames = await caches.keys();
 	await Promise.all(
-		cachedNames.filter((name) => name !== staticCacheName).map((name) => caches.delete(name)),
+		cachedNames
+			.filter((name) => name !== staticCacheName)
+			.filter((name) => name !== dynamicCacheName)
+			.map((name) => caches.delete(name)),
 	);
 });
 
 // Request Cache
 self.addEventListener('fetch', (e) => {
-	e.respondWith(cacheFirst(e.request));
+	const { request } = e;
+
+	if (URL.origin === location.origin) {
+		e.respondWith(cacheFirst(request));
+	} else {
+		e.respondWith(networkFirst(request));
+	}
 });
 
 async function cacheFirst(request) {
@@ -33,9 +52,14 @@ async function cacheFirst(request) {
 	return cached ?? (await fetch(request));
 }
 
-// async
-/* 	const cache = await caches.open(staticCacheName);
-	await cache.addAll(assetUrls); */
-// console.log('[SW]: install');
-// console.log('[SW]: activate');
-// console.log('fetch', e.request.url);
+async function networkFirst(request) {
+	const cache = await caches.open(dynamicCacheName);
+	try {
+		const response = await fetch(request);
+		await cache.put(request, response.clone());
+		return response;
+	} catch (e) {
+		const cached = await cache.match(request);
+		return cached ?? (await caches.match('/offline.html'));
+	}
+}
