ff568fffb6911b318ecc8be5a909aa07f9a9c6e5
arntmk <oleg.semchuk95@gmail.com>
2023-08-13 13:22:17 +0300

    test: update _accordion.js and _accordion.scss

--- src/assets/tests/js/_accordion.js
+++ src/assets/tests/js/_accordion.js
@@ -1,15 +1,15 @@
 /* ____________________________________________ */
-// ===Accordion===
+// ==AccordionNative==
 
 const accordionsController = function () {
 	const accordion = document.querySelectorAll('details');
 
 	if (accordion) {
+		// Animation
 		accordion.forEach((summary) => {
 			summary.addEventListener('click', (e) => {
 				const self = e.currentTarget;
 				const content = self.querySelector('.accordion-content');
-
 				if (self.hasAttribute('open')) {
 					content.style.maxHeight = '';
 				} else {
@@ -40,3 +40,32 @@ const accordionsController = function () {
 	}
 };
 accordionsController();
+
+// Animation | NEW
+function setDetailsHeight(selector, wrapper = document) {
+	const setHeight = (detail, open = false) => {
+		detail.open = open;
+		const rect = detail.getBoundingClientRect();
+		detail.dataset.width = rect.width;
+		detail.style.setProperty(open ? `--expanded` : `--collapsed`, `${rect.height}px`);
+	};
+	const details = wrapper.querySelectorAll(selector);
+	const RO = new ResizeObserver((entries) => {
+		return entries.forEach((entry) => {
+			const detail = entry.target;
+			const width = parseInt(detail.dataset.width, 10);
+			if (width !== entry.contentRect.width) {
+				detail.removeAttribute('style');
+				setHeight(detail);
+				setHeight(detail, true);
+				detail.open = false;
+			}
+		});
+	});
+	details.forEach((detail) => {
+		RO.observe(detail);
+	});
+}
+
+/* Run it */
+setDetailsHeight('details');
