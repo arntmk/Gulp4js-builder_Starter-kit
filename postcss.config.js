ad94dad1f0fa540b0a8d9f3d55458579e18932d3
arntmk <oleg.semchuk95@gmail.com>
2023-08-16 11:47:11 +0300

    build(deps): update package-lock.json, package.json and postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
 import pxtorem from 'postcss-pxtorem';
 import autoprefixer from 'autoprefixer';
 import mergerules from 'postcss-merge-rules';
+import mergelonghand from 'postcss-merge-longhand';
+import mergeidents from 'postcss-merge-idents';
 
 const postcssConfig = [
 	mergerules(),
+	mergelonghand(),
+	mergeidents(),
 	autoprefixer({ cascade: false, grid: true }),
 	pxtorem({ rootValue: 16, propWhiteList: ['*'] }),
 ];
