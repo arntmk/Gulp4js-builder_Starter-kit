5d82e17f843c63c76135fe3897a0b1a01cab9c11
arntmk <oleg.semchuk95@gmail.com>
2024-04-07 14:44:39 +0300

    update _accordion.js and script.js

--- src_components/accordion/css.native/script.js
+++ src_components/accordion/css.native/script.js
@@ -18,6 +18,37 @@ const accordionsController = function () {
 			});
 		}); */
 
+		/* ____________________________________________ */
+		// https://linkedlist.ch/animate_details_element_60/
+		// Animation | NEW
+		document.querySelectorAll('summary').forEach((element) =>
+			element.addEventListener('click', (event) => {
+				const detailsElement = event.target.parentElement;
+				const contentElement = event.target.nextElementSibling;
+
+				if (contentElement.classList.contains('animation')) {
+					contentElement.classList.remove('animation', 'collapsing');
+					void element.offsetWidth;
+					return;
+				}
+
+				const onAnimationEnd = (cb) => contentElement.addEventListener('animationend', cb, { once: true });
+
+				requestAnimationFrame(() => contentElement.classList.add('animation'));
+				onAnimationEnd(() => contentElement.classList.remove('animation'));
+
+				const isDetailsOpen = detailsElement.getAttribute('open') !== null;
+				if (isDetailsOpen) {
+					event.preventDefault();
+					contentElement.classList.add('collapsing');
+					onAnimationEnd(() => {
+						detailsElement.removeAttribute('open');
+						contentElement.classList.remove('collapsing');
+					});
+				}
+			}),
+		);
+
 		// Клик снаружи аркадиона. Закрыть аркадион.
 		accordion.forEach((closeDetails) => {
 			document.addEventListener('click', (e) => {
@@ -40,34 +71,3 @@ const accordionsController = function () {
 	}
 };
 accordionsController();
-
-/* ____________________________________________ */
-// https://linkedlist.ch/animate_details_element_60/
-// Animation | NEW
-document.querySelectorAll('summary').forEach((element) =>
-	element.addEventListener('click', (event) => {
-		const detailsElement = event.target.parentElement;
-		const contentElement = event.target.nextElementSibling;
-
-		if (contentElement.classList.contains('animation')) {
-			contentElement.classList.remove('animation', 'collapsing');
-			void element.offsetWidth;
-			return;
-		}
-
-		const onAnimationEnd = (cb) => contentElement.addEventListener('animationend', cb, { once: true });
-
-		requestAnimationFrame(() => contentElement.classList.add('animation'));
-		onAnimationEnd(() => contentElement.classList.remove('animation'));
-
-		const isDetailsOpen = detailsElement.getAttribute('open') !== null;
-		if (isDetailsOpen) {
-			event.preventDefault();
-			contentElement.classList.add('collapsing');
-			onAnimationEnd(() => {
-				detailsElement.removeAttribute('open');
-				contentElement.classList.remove('collapsing');
-			});
-		}
-	}),
-);
